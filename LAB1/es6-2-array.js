e9d6393a354f4a8ad358b2bd07ddaaf1dcb63115
suparaturairam <suparaturairam@gmail.com>
2021-11-29 21:32:17 +0700

    ok

--- LAB1/es6-2-array.js
+++ LAB1/es6-2-array.js
@@ -59,3 +59,28 @@ console.log(over15);
 function over15fn(value) {
     return value > 15;
 }
+let sum = numbs.reduce(addNumbs);
+console.log(sum);
+
+function addNumbs(total,value) {
+    return total + value;
+}
+let over10 = numbs.every(over10fn)
+console.log(over10);
+
+function over10fn(value) {
+    return value > 10;
+}
+
+let someOver10 = numbs.some(over10fn);
+console.log(someOver10);
+
+let first = numbs.find(over10fn)
+console.log(first);
+
+let firstIndex = numbs.findIndex(over10fn);
+console.log(firstIndex);
+
+const alphabet = "ABCDEF";
+let arrAlph = Array.from(alphabet);
+console.log(arrAlph);
